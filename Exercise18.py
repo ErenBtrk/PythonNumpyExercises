812b9cfe036cd17c6669e8d147f2428523de4c4e
= <=>
2021-06-19 18:39:43 +0300

    Exercise 18

---
+++ Exercise18.py
@@ -0,0 +1,10 @@
+'''
+18. Write a NumPy program to generate an array of 15 random numbers from a standard normal distribution. 
+
+'''
+
+import numpy as np
+
+np_array = np.random.normal(0,100,15)
+
+print(np_array)
