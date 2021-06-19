6e2220d725fb3aa60880eead46523143cc30a7e5
= <=>
2021-06-19 18:27:07 +0300

    Exercise 13

---
+++ Exercise13.py
@@ -0,0 +1,14 @@
+'''
+13. Write a NumPy program to create an array of 10 zeros,10 ones, 10 fives. 
+
+'''
+
+import numpy as np
+
+np_array1 = np.zeros(10)
+np_array2 = np.ones(10)
+np_array3 = np.ones(10,int)*5
+
+print(np_array1)
+print(np_array2)
+print(np_array3)
