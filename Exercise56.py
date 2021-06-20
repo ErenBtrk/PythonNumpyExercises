f901216515dd834118983350b23e3b2cc5630c7a
= <=>
2021-06-20 21:58:10 +0300

    Exercise 56

---
+++ Exercise56.py
@@ -0,0 +1,10 @@
+'''
+56. Write a NumPy program to create a three-dimension array with shape (3,5,4) and set to a variable.
+
+'''
+
+import numpy as np
+
+np_matrix = np.zeros(60).reshape(3,5,4)
+
+print(np_matrix)
