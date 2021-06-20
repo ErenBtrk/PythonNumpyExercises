3c047b527b840c7bad8621186e95aa8779351e2a
= <=>
2021-06-20 05:11:59 +0300

    Exercise 23

---
+++ Exercise23.py
@@ -0,0 +1,11 @@
+'''
+23. Write a NumPy program to create a vector of length
+5 filled with arbitrary integers from 0 to 10. 
+ 
+'''
+
+import numpy as np
+
+np_vector = np.random.randint(0,11,5)
+
+print(np_vector)
