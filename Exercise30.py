d0bc9b62ecb22db3df78667ea94298ae196df448
= <=>
2021-06-20 05:27:38 +0300

    Exercise 30

---
+++ Exercise30.py
@@ -0,0 +1,11 @@
+'''
+30. Write a NumPy program to create a 4x4 matrix in which
+0 and 1 are staggered, with zeros on the main diagonal. 
+
+'''
+
+import numpy as np
+x = np.zeros((4, 4))
+x[::2, 1::2] = 1
+x[1::2, ::2] = 1
+print(x)
