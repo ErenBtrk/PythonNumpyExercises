999496f4456789deabd1be04cf815342ac99604f
= <=>
2021-06-20 21:12:38 +0300

    Exercise 45

---
+++ Exercise45.py
@@ -0,0 +1,13 @@
+'''
+45. Write a NumPy program to create one-dimensional array of single, two and three digit numbers.
+
+'''
+
+import numpy as np
+
+np_array1 = np.arange(0,10)
+np_array2 = np.arange(10,100)
+np_array3 = np.arange(100,1000)
+print(np_array1)
+print(np_array2)
+print(np_array3)
