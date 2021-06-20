bcd4ddcfcad8a59d3eee4b9ebd33bdf1cf313128
= <=>
2021-06-20 05:14:07 +0300

    Exercise 24

---
+++ Exercise24.py
@@ -0,0 +1,12 @@
+'''
+24. Write a NumPy program to multiply the values of two given vectors.
+
+'''
+
+import numpy as np
+
+np_vector1 = np.array([1,2,3,4,5])
+np_vector2 = np.array([3,3,3,3,3])
+
+
+print(np_vector1 * np_vector2)
